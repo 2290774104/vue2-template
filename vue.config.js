460e43f9e2690151d383b3b34b1497e0089844a3
唐皓晨 <2290774104@qq.com>
2022-06-27 13:49:45 +0800

    chore: 兼容ie

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,34 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
 module.exports = defineConfig({
-  transpileDependencies: true,
+  lintOnSave: false,
+  publicPath: './',
+  configureWebpack: {
+    resolve: {
+      extensions: ['.js', '.ts', '.vue', '.json']
+    }
+  },
+  chainWebpack: config => {
+    // These are some necessary steps changing the default webpack config of the Vue CLI
+    // that need to be changed in order for Typescript based components to generate their
+    // declaration (.d.ts) files.
+    //
+    config.resolve.alias
+      .set('@', path.resolve('src'))
+      .set('types', path.resolve('types'));
+    // Discussed here https://github.com/vuejs/vue-cli/issues/1081
+    if (process.env.NODE_ENV === 'production') {
+      config.module.rule('ts').uses.delete('cache-loader');
+      config.module
+        .rule('ts')
+        .use('ts-loader')
+        .loader('ts-loader')
+        .tap(opts => {
+          opts.transpileOnly = false;
+          opts.happyPackMode = false;
+          return opts;
+        });
+    }
+  },
+  parallel: false
 });
