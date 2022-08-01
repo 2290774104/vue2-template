c5f825f479df6ebf178faa3f9ec56041bf15d7ec
唐皓晨 <2290774104@qq.com>
2022-08-01 15:26:46 +0800

    chore: 增加过滤器

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import Vue from 'vue';
+import Vue, { DirectiveOptions } from 'vue';
 
 import 'normalize.css';
 
@@ -7,6 +7,13 @@ import '@/styles/index.scss'; // global css
 import App from './App.vue';
 
 import '@/icons';
+
+import * as filters from '@/filters';
+
+// 注册过滤函数
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, (filters as { [key: string]: Function })[key]);
+});
 
 Vue.config.productionTip = false;
 
